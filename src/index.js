949a6aa6b964ca2b607b32c1f5063e715ae7d4ea
jonathanmunamire <jonathanmunamire@gmail.com>
2022-11-11 10:51:01 +0200

    create Js File

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import _ from 'lodash';
 import './style.css';
 
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  })
+})
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//   btn.addEventListener('click', function(e){
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   })
+// })
